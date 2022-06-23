606feca0eaab5c9aa5442e848b6d40b516c5ed37
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-06-23 18:05:07 -0500

    fix annotations in repository

--- src/main/java/edu/miu/springdata/lab3/repositories/CategoryRepo.java
+++ src/main/java/edu/miu/springdata/lab3/repositories/CategoryRepo.java
@@ -2,6 +2,8 @@ package edu.miu.springdata.lab3.repositories;
 
 import edu.miu.springdata.lab3.entity.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepo extends CrudRepository<Category, Integer> {
 }
--- src/main/java/edu/miu/springdata/lab3/repositories/UserRepo.java
+++ src/main/java/edu/miu/springdata/lab3/repositories/UserRepo.java
@@ -2,6 +2,8 @@ package edu.miu.springdata.lab3.repositories;
 
 import edu.miu.springdata.lab3.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends CrudRepository<User, Integer> {
 }
