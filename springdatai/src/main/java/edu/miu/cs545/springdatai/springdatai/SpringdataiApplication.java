d576112e8dbc700c0eac13e3c7400431f81835cc
wizardofwaz8282 <104391698+wizardofwaz8282@users.noreply.github.com>
2022-06-23 14:37:32 -0500

    Initial Commit and creation of folders

---
+++ springdatai/src/main/java/edu/miu/cs545/springdatai/springdatai/SpringdataiApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.springdatai.springdatai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataiApplication.class, args);
+	}
+
+}
---
+++ springdatai/src/test/java/edu/miu/cs545/springdatai/springdatai/SpringdataiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.springdatai.springdatai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
