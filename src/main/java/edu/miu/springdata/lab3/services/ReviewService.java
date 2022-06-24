5e33321a5255e97a86ffe0e842d05571a10f6fa3
pravash <pravash@pravash-mac.local>
2022-06-23 20:16:28 -0500

    Add review service'

--- src/main/java/edu/miu/springdata/lab3/services/ReviewService.java
+++ src/main/java/edu/miu/springdata/lab3/services/ReviewService.java
@@ -1,4 +1,15 @@
 package edu.miu.springdata.lab3.services;
 
+import edu.miu.springdata.lab3.entity.User;
+
+import java.util.List;
+
 public interface ReviewService {
+    void save(User p);
+
+    void delete(int id);
+
+    User getById(int id);
+
+    List<User> getAll();
 }
--- src/main/java/edu/miu/springdata/lab3/services/impl/ReviewServiceImpl.java
+++ src/main/java/edu/miu/springdata/lab3/services/impl/ReviewServiceImpl.java
@@ -1,15 +1,40 @@
 package edu.miu.springdata.lab3.services.impl;
 
+import edu.miu.springdata.lab3.entity.User;
 import edu.miu.springdata.lab3.repositories.ReviewRepo;
+import edu.miu.springdata.lab3.repositories.UserRepo;
 import edu.miu.springdata.lab3.services.ReviewService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Service
 @AllArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
-    @Autowired
     private final ReviewRepo reviewRepo;
+    @Override
+    public void save(User p) {
+        reviewRepo.save(p);
+    }
+
+    @Override
+    public void delete(int id) {
+        reviewRepo.deleteById(id);
+    }
+
+    @Override
+    public User getById(int id) {
+        return (User) reviewRepo.findById(id).get();
+    }
+
+    @Override
+    public List<User> getAll() {
+        return StreamSupport.stream(reviewRepo.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
 }
